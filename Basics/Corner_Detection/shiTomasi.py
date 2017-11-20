286ea134b64516f4965de20da362c678faacf2d1
Tanmay2298 <Tanmaygulati@gmail.com>
2017-11-20 11:29:02 +0530

    Finger Print scan corner detection

--- Basics/Corner_Detection/shiTomasi.py
+++ Basics/Corner_Detection/shiTomasi.py
@@ -3,7 +3,10 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-filename = '/Users/tanmaygulati/Documents/MATLAB/Image-Processing-using-Matlab/Retinal Scan Work/retina_scan.jpg'
+# 1) Retina scan
+# 2) Finger print scan
+# filename = '/Users/tanmaygulati/Documents/MATLAB/Image-Processing-using-Matlab/Retinal Scan Work/retina_scan.jpg'
+# filename = '/Users/tanmaygulati/Work/Research Project/Dr Srikanth Prabhu/Python-Image-Processing/Basics/Corner_Detection/snap2.jpg'
 img = cv2.imread(filename)
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
@@ -14,5 +17,5 @@ for i in corners:
     x,y = i.ravel()
     cv2.circle(img,(x,y),3,255,-1)
 
-cv2.imwrite("Shi-Tomasi.png", img)
+cv2.imwrite("Shi-Tomasi2.png", img)
 plt.imshow(img),plt.show()
